240347587a70d34ad724bae70179e1ec127fb216
Daniel Holoubek <danikh100@email.cz>
2023-12-30 14:32:32 +0100

    #1: Database and GraphQL init

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,41 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { join } from 'path'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule } from '@nestjs/config'
+import { ArticleModule } from './article/article.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: process.env.GRAPHQL_PLAYGROUND == 'true',
+      introspection: process.env.GRAPHQL_PLAYGROUND == 'true',
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      context: ({ req }: { req: Request }) => ({ req }),
+      sortSchema: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: (process.env.TYPEORM_CONNECTION ?? 'mysql') as any,
+      host: process.env.TYPEORM_HOST,
+      port: Number(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: process.env.TYPEORM_ENTITIES ? process.env.TYPEORM_ENTITIES.split(',') : [],
+      migrationsTableName: process.env.TYPEORM_MIGRATIONS_TABLE_NAME,
+      migrations: process.env.TYPEORM_MIGRATIONS ? process.env.TYPEORM_MIGRATIONS.split(',') : [],
+      migrationsRun: process.env.TYPEORM_MIGRATIONS_RUN?.toLowerCase().includes('true'),
+      logging: 'all',
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
