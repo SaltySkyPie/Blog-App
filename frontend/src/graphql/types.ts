a737a024af9f96ee42cfa52f46a11990497e3178
Daniel Holoubek <danikh100@email.cz>
2024-01-08 00:25:01 +0100

    #8: Share button, lang mutations, routes, helper functions

--- frontend/src/graphql/types.ts
+++ frontend/src/graphql/types.ts
@@ -32,10 +32,8 @@ export type Article = {
 };
 
 export enum ArticleState {
-  Deleted = 'DELETED',
   Draft = 'DRAFT',
   Hidden = 'HIDDEN',
-  Private = 'PRIVATE',
   Published = 'PUBLISHED'
 }
 
@@ -43,7 +41,7 @@ export type CreateArticleInput = {
   content: Scalars['String']['input'];
   imageUrl?: InputMaybe<Scalars['String']['input']>;
   perex: Scalars['String']['input'];
-  state?: ArticleState;
+  state: ArticleState;
   title: Scalars['String']['input'];
 };
 
@@ -87,6 +85,7 @@ export type Query = {
   article: Article;
   articles: Array<Article>;
   user: User;
+  userArticle: Article;
   userArticles: Array<Article>;
 };
 
@@ -98,6 +97,11 @@ export type QueryArticleArgs = {
 
 export type QueryUserArgs = {
   id: Scalars['Int']['input'];
+};
+
+
+export type QueryUserArticleArgs = {
+  id: Scalars['ID']['input'];
 };
 
 export type UpdateArticleInput = {
@@ -128,12 +132,158 @@ export type User = {
   username: Scalars['String']['output'];
 };
 
+export type CreateArticleMutationVariables = Exact<{
+  input: CreateArticleInput;
+}>;
+
+
+export type CreateArticleMutation = { __typename?: 'Mutation', createArticle: { __typename?: 'Article', id: string, state: ArticleState } };
+
+export type UpdateArticleMutationVariables = Exact<{
+  input: UpdateArticleInput;
+}>;
+
+
+export type UpdateArticleMutation = { __typename?: 'Mutation', updateArticle: { __typename?: 'Article', id: string, state: ArticleState } };
+
+export type GetUserArticleQueryVariables = Exact<{
+  id: Scalars['ID']['input'];
+}>;
+
+
+export type GetUserArticleQuery = { __typename?: 'Query', userArticle: { __typename?: 'Article', id: string, title: string, perex: string, createdAt: any, imageUrl?: string | null, content: string } };
+
 export type GetArticlesQueryVariables = Exact<{ [key: string]: never; }>;
 
 
 export type GetArticlesQuery = { __typename?: 'Query', articles: Array<{ __typename?: 'Article', id: string, title: string, perex: string, createdAt: any, imageUrl?: string | null, user: { __typename?: 'User', id: string, firstName: string, lastName: string, middleName?: string | null } }> };
 
+export type GetUserArticlesQueryVariables = Exact<{ [key: string]: never; }>;
 
+
+export type GetUserArticlesQuery = { __typename?: 'Query', userArticles: Array<{ __typename?: 'Article', id: string, title: string, createdAt: any, updatedAt: any, state: ArticleState }> };
+
+export type GetArticleQueryVariables = Exact<{
+  id: Scalars['ID']['input'];
+}>;
+
+
+export type GetArticleQuery = { __typename?: 'Query', article: { __typename?: 'Article', id: string, title: string, perex: string, createdAt: any, imageUrl?: string | null, content: string, user: { __typename?: 'User', id: string, firstName: string, lastName: string, middleName?: string | null } } };
+
+
+export const CreateArticleDocument = gql`
+    mutation createArticle($input: CreateArticleInput!) {
+  createArticle(createArticleInput: $input) {
+    id
+    state
+  }
+}
+    `;
+export type CreateArticleMutationFn = Apollo.MutationFunction<CreateArticleMutation, CreateArticleMutationVariables>;
+
+/**
+ * __useCreateArticleMutation__
+ *
+ * To run a mutation, you first call `useCreateArticleMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreateArticleMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [createArticleMutation, { data, loading, error }] = useCreateArticleMutation({
+ *   variables: {
+ *      input: // value for 'input'
+ *   },
+ * });
+ */
+export function useCreateArticleMutation(baseOptions?: Apollo.MutationHookOptions<CreateArticleMutation, CreateArticleMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<CreateArticleMutation, CreateArticleMutationVariables>(CreateArticleDocument, options);
+      }
+export type CreateArticleMutationHookResult = ReturnType<typeof useCreateArticleMutation>;
+export type CreateArticleMutationResult = Apollo.MutationResult<CreateArticleMutation>;
+export type CreateArticleMutationOptions = Apollo.BaseMutationOptions<CreateArticleMutation, CreateArticleMutationVariables>;
+export const UpdateArticleDocument = gql`
+    mutation updateArticle($input: UpdateArticleInput!) {
+  updateArticle(updateArticleInput: $input) {
+    id
+    state
+  }
+}
+    `;
+export type UpdateArticleMutationFn = Apollo.MutationFunction<UpdateArticleMutation, UpdateArticleMutationVariables>;
+
+/**
+ * __useUpdateArticleMutation__
+ *
+ * To run a mutation, you first call `useUpdateArticleMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useUpdateArticleMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [updateArticleMutation, { data, loading, error }] = useUpdateArticleMutation({
+ *   variables: {
+ *      input: // value for 'input'
+ *   },
+ * });
+ */
+export function useUpdateArticleMutation(baseOptions?: Apollo.MutationHookOptions<UpdateArticleMutation, UpdateArticleMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<UpdateArticleMutation, UpdateArticleMutationVariables>(UpdateArticleDocument, options);
+      }
+export type UpdateArticleMutationHookResult = ReturnType<typeof useUpdateArticleMutation>;
+export type UpdateArticleMutationResult = Apollo.MutationResult<UpdateArticleMutation>;
+export type UpdateArticleMutationOptions = Apollo.BaseMutationOptions<UpdateArticleMutation, UpdateArticleMutationVariables>;
+export const GetUserArticleDocument = gql`
+    query getUserArticle($id: ID!) {
+  userArticle(id: $id) {
+    id
+    title
+    perex
+    createdAt
+    imageUrl
+    content
+  }
+}
+    `;
+
+/**
+ * __useGetUserArticleQuery__
+ *
+ * To run a query within a React component, call `useGetUserArticleQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetUserArticleQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetUserArticleQuery({
+ *   variables: {
+ *      id: // value for 'id'
+ *   },
+ * });
+ */
+export function useGetUserArticleQuery(baseOptions: Apollo.QueryHookOptions<GetUserArticleQuery, GetUserArticleQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetUserArticleQuery, GetUserArticleQueryVariables>(GetUserArticleDocument, options);
+      }
+export function useGetUserArticleLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetUserArticleQuery, GetUserArticleQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetUserArticleQuery, GetUserArticleQueryVariables>(GetUserArticleDocument, options);
+        }
+export function useGetUserArticleSuspenseQuery(baseOptions?: Apollo.SuspenseQueryHookOptions<GetUserArticleQuery, GetUserArticleQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useSuspenseQuery<GetUserArticleQuery, GetUserArticleQueryVariables>(GetUserArticleDocument, options);
+        }
+export type GetUserArticleQueryHookResult = ReturnType<typeof useGetUserArticleQuery>;
+export type GetUserArticleLazyQueryHookResult = ReturnType<typeof useGetUserArticleLazyQuery>;
+export type GetUserArticleSuspenseQueryHookResult = ReturnType<typeof useGetUserArticleSuspenseQuery>;
+export type GetUserArticleQueryResult = Apollo.QueryResult<GetUserArticleQuery, GetUserArticleQueryVariables>;
 export const GetArticlesDocument = gql`
     query getArticles {
   articles {
@@ -183,3 +333,97 @@ export type GetArticlesQueryHookResult = ReturnType<typeof useGetArticlesQuery>;
 export type GetArticlesLazyQueryHookResult = ReturnType<typeof useGetArticlesLazyQuery>;
 export type GetArticlesSuspenseQueryHookResult = ReturnType<typeof useGetArticlesSuspenseQuery>;
 export type GetArticlesQueryResult = Apollo.QueryResult<GetArticlesQuery, GetArticlesQueryVariables>;
+export const GetUserArticlesDocument = gql`
+    query getUserArticles {
+  userArticles {
+    id
+    title
+    createdAt
+    updatedAt
+    state
+  }
+}
+    `;
+
+/**
+ * __useGetUserArticlesQuery__
+ *
+ * To run a query within a React component, call `useGetUserArticlesQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetUserArticlesQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetUserArticlesQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useGetUserArticlesQuery(baseOptions?: Apollo.QueryHookOptions<GetUserArticlesQuery, GetUserArticlesQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetUserArticlesQuery, GetUserArticlesQueryVariables>(GetUserArticlesDocument, options);
+      }
+export function useGetUserArticlesLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetUserArticlesQuery, GetUserArticlesQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetUserArticlesQuery, GetUserArticlesQueryVariables>(GetUserArticlesDocument, options);
+        }
+export function useGetUserArticlesSuspenseQuery(baseOptions?: Apollo.SuspenseQueryHookOptions<GetUserArticlesQuery, GetUserArticlesQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useSuspenseQuery<GetUserArticlesQuery, GetUserArticlesQueryVariables>(GetUserArticlesDocument, options);
+        }
+export type GetUserArticlesQueryHookResult = ReturnType<typeof useGetUserArticlesQuery>;
+export type GetUserArticlesLazyQueryHookResult = ReturnType<typeof useGetUserArticlesLazyQuery>;
+export type GetUserArticlesSuspenseQueryHookResult = ReturnType<typeof useGetUserArticlesSuspenseQuery>;
+export type GetUserArticlesQueryResult = Apollo.QueryResult<GetUserArticlesQuery, GetUserArticlesQueryVariables>;
+export const GetArticleDocument = gql`
+    query getArticle($id: ID!) {
+  article(id: $id) {
+    id
+    title
+    perex
+    createdAt
+    imageUrl
+    content
+    user {
+      id
+      firstName
+      lastName
+      middleName
+    }
+  }
+}
+    `;
+
+/**
+ * __useGetArticleQuery__
+ *
+ * To run a query within a React component, call `useGetArticleQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetArticleQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetArticleQuery({
+ *   variables: {
+ *      id: // value for 'id'
+ *   },
+ * });
+ */
+export function useGetArticleQuery(baseOptions: Apollo.QueryHookOptions<GetArticleQuery, GetArticleQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetArticleQuery, GetArticleQueryVariables>(GetArticleDocument, options);
+      }
+export function useGetArticleLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetArticleQuery, GetArticleQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetArticleQuery, GetArticleQueryVariables>(GetArticleDocument, options);
+        }
+export function useGetArticleSuspenseQuery(baseOptions?: Apollo.SuspenseQueryHookOptions<GetArticleQuery, GetArticleQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useSuspenseQuery<GetArticleQuery, GetArticleQueryVariables>(GetArticleDocument, options);
+        }
+export type GetArticleQueryHookResult = ReturnType<typeof useGetArticleQuery>;
+export type GetArticleLazyQueryHookResult = ReturnType<typeof useGetArticleLazyQuery>;
+export type GetArticleSuspenseQueryHookResult = ReturnType<typeof useGetArticleSuspenseQuery>;
+export type GetArticleQueryResult = Apollo.QueryResult<GetArticleQuery, GetArticleQueryVariables>;
