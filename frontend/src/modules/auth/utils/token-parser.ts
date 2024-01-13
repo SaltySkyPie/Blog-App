9fa82710bfecbcca4940935456031535f5817f85
Daniel Holoubek <danikh100@email.cz>
2024-01-13 16:58:58 +0100

    #11: Pagination on ArticleList

--- frontend/src/modules/auth/utils/token-parser.ts
+++ frontend/src/modules/auth/utils/token-parser.ts
@@ -12,7 +12,6 @@ export function parseToken(jwt: string): TokenInfo {
   const base64Url = jwt.split('.')[1]
   const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
   const decodedPayload = decodeBase64WithDiacritics(base64)
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return JSON.parse(decodedPayload)
 }
 
