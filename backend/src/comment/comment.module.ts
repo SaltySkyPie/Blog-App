747634e62c76d13b208c5432bb00f920293bc39d
Daniel Holoubek <danikh100@email.cz>
2024-01-13 17:06:31 +0100

    #12: Circular dependency fix

--- backend/src/comment/comment.module.ts
+++ backend/src/comment/comment.module.ts
@@ -1,13 +1,14 @@
 import { ArticleModule } from '@app/article/article.module'
 import { UserModule } from '@app/user/user.module'
-import { Module } from '@nestjs/common'
+import { Module, forwardRef } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { CommentResolver } from './comment.resolver'
 import { CommentService } from './comment.service'
 import { Comment } from './entities/comment.entity'
+import { VoteModule } from '@app/vote/vote.module'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Comment]), UserModule, ArticleModule],
+  imports: [TypeOrmModule.forFeature([Comment]), UserModule, ArticleModule, forwardRef(() => VoteModule)],
   providers: [CommentResolver, CommentService],
   exports: [CommentService],
 })
