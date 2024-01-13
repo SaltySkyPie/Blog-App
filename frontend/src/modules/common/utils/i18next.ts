9bef8990a2c5ea77d3f09b6db23856f2abca5bd1
Daniel Holoubek <danikh100@email.cz>
2024-01-13 16:54:21 +0100

    #11: Added helper function to display "x time ago" strings

--- frontend/src/modules/common/utils/i18next.ts
+++ frontend/src/modules/common/utils/i18next.ts
@@ -3,21 +3,30 @@ import { initReactI18next } from 'react-i18next'
 
 import messagesCs from '@app/assets/localization/cs.json'
 import messagesEn from '@app/assets/localization/en.json'
+import timeAgoCs from 'timeago.js/lib/lang/cs'
+import timeAgoEn from 'timeago.js/lib/lang/en_US'
 
-export async function initI18next() {
-  await i18n.use(initReactI18next).init({
-    resources: {
+const resources = {
       en: {
         translation: messagesEn,
+        timeAgo: timeAgoEn,
       },
       cs: {
         translation: messagesCs,
+        timeAgo: timeAgoCs,
       },
-    },
+    }
+
+export async function initI18next() {
+  await i18n.use(initReactI18next).init({
+    resources: resources,
     lng: localStorage.getItem('lang') || 'en',
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false,
+    },
+    cache: {
+      enabled: true,
     },
   })
 }
@@ -26,11 +35,15 @@ export function getLanguage() {
   return i18n.language
 }
 
+export function getLanguageResource() {
+  return resources[i18n.language as keyof typeof resources]
+}
+
 export function setLanguage(lang: string) {
   localStorage.setItem('lang', lang)
   return i18n.changeLanguage(lang)
 }
 
 export function getLanguages() {
-  return i18n.languages
+  return Object.keys(resources)
 }
